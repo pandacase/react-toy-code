3a31371b94bffce10358df1b84014b3bd6a40221
pandacase <2227119810@qq.com>
2024-10-21 17:00:43 +0800

    food list component

--- food-app/src/App.jsx
+++ food-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import FoodList from "./components/FoodList";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
@@ -9,9 +10,7 @@ function App() {
         foodData={foodData}
         setFoodData={setFoodData}
       />
-      {foodData.map((food) => (
-        <h1>{food.title}</h1>
-      ))}
+      <FoodList foodData={foodData} />
     </div>
   );
 }
