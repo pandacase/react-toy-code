5e55b9787de4f410e19015957963dbb55865aa84
pandacase <2227119810@qq.com>
2024-10-22 16:31:30 +0800

    udpate app

--- food-app/src/App.jsx
+++ food-app/src/App.jsx
@@ -1,16 +1,33 @@
+import "./App.css";
 import { useState } from "react";
 import Search from "./components/Search";
-import FoodList from "./components/FoodList";
+import FoodList from "./components/food-list/FoodList";
+import Nav from "./components/Nav";
+import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
+import FoodDetails from "./components/food-detail/FoodDetails";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("658615");
   return (
     <div className="App">
+      <Nav />
       <Search
         foodData={foodData}
         setFoodData={setFoodData}
       />
-      <FoodList foodData={foodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList
+            foodData={foodData}
+            setFoodId={setFoodId}
+          />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
