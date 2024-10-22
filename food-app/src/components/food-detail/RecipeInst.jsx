ea5955a7ce01f2bc2158c0e2ca34c228c8f96774
pandacase <2227119810@qq.com>
2024-10-22 16:38:28 +0800

    recipe instructions

---
+++ food-app/src/components/food-detail/RecipeInst.jsx
@@ -0,0 +1,17 @@
+import styles from "./recipeinst.module.css"
+
+export default function RecipeInst() {
+  return (
+    <div className={styles.recipeInst}>
+      <ol>
+        {isLoaded ? (
+          food.analyzedInstructions[0].steps.map((step) => (
+            <li key={step.number}>{step.step}</li>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </ol>
+    </div>
+  );
+}
