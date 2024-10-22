15d3af70faf6ccbc447790d7a2fcfece3dc4c16f
pandacase <2227119810@qq.com>
2024-10-22 12:25:08 +0800

    foodItem

--- food-app/src/components/FoodItem.jsx
+++ food-app/src/components/FoodItem.jsx
@@ -1,9 +1,22 @@
+import styles from "./fooditem.module.css";
+
 export default function FoodItem({ food }) {
   return (
-    <div>
-      <img src={food.image} alt="img" />
-      <h1>{food.title}</h1>
-      <button>View Recipe</button>
+    <div className={styles.itemContainer}>
+      <img
+        className={styles.itemImage}
+        src={food.image}
+        alt="img"
+      />
+      <div className={styles.itenContent}>
+        <p className={styles.itemName}>{food.title}</p>
+      </div>
+
+      <div className={styles.btnConatiner}>
+        <button className={styles.itemBtn}>
+          View Recipe
+        </button>
+      </div>
     </div>
   );
 }
